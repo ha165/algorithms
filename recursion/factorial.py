724848f138bf0e86bf73632b33f8ffa1b2088775
ha165 <lumumbaharmony@gmail.com>
2024-07-30 11:46:40 +0300

    Update factorial.py

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,2 +1,6 @@
 def factorial(n):
-    return n*(n-1)   #recursive case
+    if n in [0,1]:              # the base case
+        return 1
+    else:
+        return n*factorial(n-1)   #recursive case
+print(factorial(3))
