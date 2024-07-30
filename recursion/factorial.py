55954885a44c39ac8c52505777f93ecdb736b61e
ha165 <lumumbaharmony@gmail.com>
2024-07-30 11:52:56 +0300

    Update factorial.py

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,4 +1,5 @@
 def factorial(n):
+      # Assertion to check if the input is a positive integer
     assert n>=0 and int(n) == n , 'The Number Must be positive integer Only!'
     if n in [0,1]:              # the base case
         return 1
