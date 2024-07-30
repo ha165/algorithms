97d7fe215d6e205b3cc596eff454e4509d3c1124
ha165 <lumumbaharmony@gmail.com>
2024-07-30 14:39:51 +0300

    Update sum_of_digits_positiveInteger.py

--- recursion/sum_of_digits_positiveInteger.py
+++ recursion/sum_of_digits_positiveInteger.py
@@ -1,7 +1,8 @@
 # How to find the sum of digits of positive integer number using recursion
 def sumofDigits(n):
-    if n == 0:
+    assert n>0 and int(n) == 0, 'The number must be an a positive inteer!'    #unconditional case
+    if n == 0:                                           # CASE BASE
         return 0
     else:
-        return int(n%10) + sumofDigits(int(n/10))
+        return int(n%10) + sumofDigits(int(n/10))        # RECURSIVE CASE
 print(sumofDigits(15))
