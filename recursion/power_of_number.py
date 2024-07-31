53b09206f813895b66fba702fc6419674f0fc262
ha165 <lumumbaharmony@gmail.com>
2024-07-31 09:46:10 +0300

    Update power_of_number.py

--- recursion/power_of_number.py
+++ recursion/power_of_number.py
@@ -1,3 +1,12 @@
 #How to calculate power of a number using recursion
 def power(base,exp):
-    return base * power(base,exp-1)   #recursive case
+    
+    #base case
+    if exp == 0:
+        return 1
+    if exp == 1:
+        return base;
+    
+    #recursive case
+    return base * power(base,exp-1)   
+print(power(2,4))
