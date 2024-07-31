064a42e2f42e35a8c4b3b038906d96133904f7dd
ha165 <lumumbaharmony@gmail.com>
2024-07-31 10:04:23 +0300

    Update GCD_of_twonumbers.py

--- GCD_of_twonumbers.py
+++ GCD_of_twonumbers.py
@@ -1,7 +1,11 @@
 # How to find GCD(greatest common divisor) of two numbers using recursion?
 def gcd(a,b):
     #unintentional case
-    
+    assert int(a) == a and int(b) == b,'The number must be Integer'
+    if a<0:
+        a = -1 * a
+    if b<0:
+        b = -1 * b
     #base case
     if b == 0:
         return a
