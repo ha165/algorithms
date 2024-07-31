c7e9f657c54d66f779c6c791d9d22a2579dccc96
ha165 <lumumbaharmony@gmail.com>
2024-07-31 10:35:51 +0300

    Update decimal_binary.py

--- recursion/decimal_binary.py
+++ recursion/decimal_binary.py
@@ -1,2 +1,8 @@
 # How to convert a number from decimal tp binary
-def 
+def decimalbinary(n):
+    if n == 0:            #base case
+        return 0
+    else:
+        return n%2 + 10 * decimalbinary(int(n/2))     #recursive case
+
+print(decimalbinary(10))
